2fae449769eba72bfa7ed623030b7da815faa113
Abe Diaz <aberoddia@gmail.com>
2023-05-08 14:31:58 -0400

    Database schema

---
+++ schema.sql
@@ -0,0 +1,80 @@
+CREATE DATABASE University;
+
+USE University;
+
+CREATE TABLE department(
+	dept_name VARCHAR(20) NOT NULL,
+	building INT,
+	PRIMARY KEY(dept_name)
+);
+
+CREATE TABLE student(
+	ID INT NOT NULL,
+	firstName VARCHAR(20) NOT NULL,
+	lastName VARCHAR(20) NOT NULL,
+	dept_name VARCHAR(20) NOT NULL,
+	credits INT,
+	PRIMARY KEY(ID),
+	FOREIGN KEY(dept_name) REFERENCES department(dept_name)
+);
+
+CREATE TABLE instructor(
+	ID INT NOT NULL,
+	firstName VARCHAR(20) NOT NULL,
+	lastName VARCHAR(20) NOT NULL,
+	dept_name VARCHAR(20) NOT NULL,
+	PRIMARY KEY(ID),
+	FOREIGN KEY(dept_name) REFERENCES department(dept_name)
+);
+
+CREATE TABLE course(
+	course_id INT NOT NULL,
+	title VARCHAR(20),
+	dept_name VARCHAR(20) NOT NULL,
+	credits INT,
+	PRIMARY KEY(course_id),
+	FOREIGN KEY(dept_name) REFERENCES department(dept_name)
+);
+
+CREATE TABLE section(
+	course_id INT NOT NULL,
+	sec_id INT NOT NULL,
+	semester VARCHAR(8) NOT NULL,
+	year YEAR NOT NULL,
+	building VARCHAR(3),
+	room_no INT,
+	PRIMARY KEY(course_id, sec_id, semester, year),
+	FOREIGN KEY(course_id) REFERENCES course(course_id)
+);
+
+CREATE TABLE classroom(
+	building VARCHAR(3) NOT NULL,
+	room_no INT NOT NULL,
+	cpacity INT,
+	PRIMARY KEY(building, room_no)
+);
+
+CREATE TABLE teaches(
+	ID INT NOT NULL, 
+	course_id INT NOT NULL,
+	sec_id INT NOT NULL,
+	semester VARCHAR(8) NOT NULL,
+	year YEAR NOT NULL,
+	PRIMARY KEY(ID, course_id, sec_id, semester, year),
+	FOREIGN KEY(ID) REFERENCES instructor(ID),
+	FOREIGN KEY(course_id) REFERENCES course(course_id),
+	FOREIGN KEY(course_id, sec_id, semester, year) REFERENCES section(course_id, sec_id, semester, year)
+);
+
+CREATE TABLE takes(
+	ID INT NOT NULL,
+	course_id INT NOT NULL,
+	sec_id INT NOT NULL,
+	semester VARCHAR(8) NOT NULL,
+	year YEAR NOT NULL,
+	grade VARCHAR(2),
+	PRIMARY KEY(ID, course_id, sec_id, semester, year),
+	FOREIGN KEY(ID) REFERENCES student(ID),
+	FOREIGN KEY(course_id) REFERENCES course(course_id),
+	FOREIGN KEY(course_id, sec_id, semester, year) REFERENCES section(course_id, sec_id, semester, year)
+);
