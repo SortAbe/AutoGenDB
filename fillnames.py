56e02eb46bb76af8f8d3ac12b8617e45c709b84c
Abe Diaz <aberoddia@gmail.com>
2024-01-27 22:43:24 -0500

    Attempting to fix errors. [TESTING]

--- fillnames.py
+++ fillnames.py
@@ -21,33 +21,33 @@ class Filler:
 		port=os.environ.get('db_port')
 	)
 	cursor = cnx.cursor()
-	departmentList = []
-	femaleNames = []
-	maleNames = []
-	lastNames = []
-	areaCode = {}
+	department_list = []
+	female_names = []
+	male_names = []
+	last_names = []
+	area_code = {}
 
 	def __init__(self):
 		query = 'SELECT * FROM department'
 		self.cursor.execute(query)
 		results = self.cursor.fetchall()
 		for row in results:
-			self.departmentList.append(row[0])
+			self.department_list.append(row[0])
 		with open('./lists/female.names', 'r') as femf:
 			lines = femf.readlines()
 			for line in lines:
-				self.femaleNames.append(line)
+				self.female_names.append(line)
 		with open('./lists/male.names', 'r') as malf:
 			lines = malf.readlines()
 			for line in lines:
-				self.maleNames.append(line)
+				self.male_names.append(line)
 		with open('./lists/last.names', 'r') as lasf:
 			lines = lasf.readlines()
 			for line in lines:
-				self.lastNames.append(line)
+				self.last_names.append(line)
 		with open('./lists/areac.json', 'r') as jsonf:
 			data = jsonf.read()
-			self.areaCode = json.loads(data)
+			self.area_code = json.loads(data)
 
 	def insert(self, sql, data):
 		fail = True
@@ -62,16 +62,16 @@ class Filler:
 	def fill_student(self, offset, gen=True, rows=100_000):
 		gender = gen
 		if gen:
-			flen = len(self.femaleNames)
-			random.shuffle(self.femaleNames)
+			flen = len(self.female_names)
+			random.shuffle(self.female_names)
 		else:
-			flen = len(self.maleNames)
-			random.shuffle(self.maleNames)
-		llen = len(self.lastNames)
-		dlen = len(self.departmentList)
-		random.shuffle(self.lastNames)
+			flen = len(self.male_names)
+			random.shuffle(self.male_names)
+		llen = len(self.last_names)
+		dlen = len(self.department_list)
+		random.shuffle(self.last_names)
 		key_date = datetime.date(2015, 1, 1)
-		random.shuffle(self.departmentList)
+		random.shuffle(self.department_list)
 
 		sql = "INSERT INTO student (ID, firstName, lastName, gender, \
 			dept_name, registered, credits) \
@@ -87,12 +87,12 @@ class Filler:
 			credit = random.choice(range(60))	
 			date_delta = key_date + datetime.timedelta(days=random.choice(range(3600)))
 			if gender:
-				first = self.femaleNames[i%flen].strip()
+				first = self.female_names[i%flen].strip()
 			else:
-				first = self.maleNames[i%flen].strip()
-			last = self.lastNames[i%llen].strip()
+				first = self.male_names[i%flen].strip()
+			last = self.last_names[i%llen].strip()
 
-			data = (count, first, last, gender, self.departmentList[i%dlen].strip(), date_delta.strftime('%Y-%m-%d'), credit)
+			data = (count, first, last, gender, self.department_list[i%dlen].strip(), date_delta.strftime('%Y-%m-%d'), credit)
 
 			addr = rra()
 			if 'city' not in addr:
@@ -101,7 +101,7 @@ class Filler:
 			data2 = (count, addr['address1'], addr['address2'], addr['city'], addr['state'], addr['postalCode'])
 
 			data3 = (count, (first.lower() + last.lower() + addr['postalCode'] + '@sqlu.edu'), 
-			( '(' + random.choice(self.areaCode[addr['state']]) + ')-'\
+			( '(' + random.choice(self.area_code[addr['state']]) + ')-'\
 			+ str(random.choice(range(1000,2000)))[1:] + '-' + str(random.choice(range(10000,20000)))[1:]))
 
 			self.insert(sql, data)
@@ -115,15 +115,15 @@ class Filler:
 	def fill_instructor(self, offset, rows=10_000, gen=True):
 		gender = gen
 		if gen:
-			flen = len(self.femaleNames)
-			random.shuffle(self.femaleNames)
+			flen = len(self.female_names)
+			random.shuffle(self.female_names)
 		else:
-			flen = len(self.maleNames)
-			random.shuffle(self.maleNames)
-		llen = len(self.lastNames)
-		dlen = len(self.departmentList)
-		random.shuffle(self.lastNames)
-		random.shuffle(self.departmentList)
+			flen = len(self.male_names)
+			random.shuffle(self.male_names)
+		llen = len(self.last_names)
+		dlen = len(self.department_list)
+		random.shuffle(self.last_names)
+		random.shuffle(self.department_list)
 		
 		sql = "INSERT INTO instructor \
 			(ID, firstName, lastName, gender, dept_name, salary) \
@@ -137,13 +137,13 @@ class Filler:
 		count = 0 + offset
 		for i in range(rows):
 			if gender:
-				first = self.femaleNames[i%flen].strip()
+				first = self.female_names[i%flen].strip()
 			else:
-				first = self.maleNames[i%flen].strip()
-			last = self.lastNames[i%llen].strip()
+				first = self.male_names[i%flen].strip()
+			last = self.last_names[i%llen].strip()
 			salary = random.choice(range(80_000,160_000))
 
-			data = (count, first, last, gender, self.departmentList[i%dlen].strip(), salary)
+			data = (count, first, last, gender, self.department_list[i%dlen].strip(), salary)
 
 			addr = rra()
 			if 'city' not in addr:
@@ -152,7 +152,7 @@ class Filler:
 			data2 = (count, addr['address1'], addr['address2'], addr['city'], addr['state'], addr['postalCode'])
 
 			data3 = (count, (first.lower() + last.lower() + addr['postalCode'] + '@teacher.sqlu.edu'), 
-				( '(' + random.choice(self.areaCode[addr['state']]) \
+				( '(' + random.choice(self.area_code[addr['state']]) \
 				+ ')-' + str(random.choice(range(1000,2000)))[1:] + '-' + str(random.choice(range(10000,20000)))[1:]))
 
 			self.insert(sql, data)
@@ -201,8 +201,8 @@ class Filler:
 		for dept in self.cursor.fetchall():
 			self.cursor.execute('SELECT ID FROM instructor WHERE dept_name = \"' + dept[0] + '\";')
 			teachers = self.cursor.fetchall()
-			teachNums = len(teachers)
-			teachSelect = 0
+			teach_nums = len(teachers)
+			teach_select = 0
 			self.cursor.execute('SELECT course_id FROM course WHERE dept_name = \"' + dept[0] + '\";')
 			count = 0
 			for course in self.cursor.fetchall():
@@ -211,9 +211,9 @@ class Filler:
 
 					sql = 'INSERT IGNORE INTO teaches (ID, course_id, class_id) VALUES (%s, %s, %s);'
 
-					data = (str(teachers[teachSelect % teachNums][0]), str(course[0]), str(class_[0])) 
+					data = (str(teachers[teach_select % teach_nums][0]), str(course[0]), str(class_[0])) 
 					self.insert(sql, data)
-					teachSelect += 1
+					teach_select += 1
 					count += 1
 					if count % 1000 == 0:
 						self.cnx.commit()
@@ -247,21 +247,28 @@ class Filler:
 						return
 			self.cnx.commit()
 	
-	def genOffset(self):
+	def gen_offset(self):
+		student, instructor, class_ = 0,0,0
 		self.cursor.execute('SELECT MAX(ID) FROM student;')
-		student = self.cursor.fetchone()[0] // 100_000
-		self.cursor.execute('SELECT MAX(ID) FROM instructor;')
-		instructor = self.cursor.fetchone()[0] // 10_000
-		self.cursor.execute('SELECT MAX(class_id) FROM class;')
-		cla = self.cursor.fetchone()[0] // 30_000
-		if student == None or instructor == None or cla == None:
+		student_get = self.cursor.fetchone()
+		if student_get != None:
+			student = int(student_get[0]) // 100_000
+		else:
 			return 0
-		if student > instructor and student > cla:
+		self.cursor.execute('SELECT MAX(ID) FROM instructor;')
+		instructor_get = self.cursor.fetchone()
+		if instructor_get != None:
+			instructor = int(instructor_get[0]) // 10_000
+		self.cursor.execute('SELECT MAX(class_id) FROM class;')
+		class_get = self.cursor.fetchone()
+		if class_get != None:
+			class_ = int(class_get[0]) // 30_000
+		if student > instructor and student > class_ :
 			return student + 1
-		if instructor > cla:
+		if instructor > class_:
 			return instructor + 1
 		else:
-			return cla + 1
+			return class_ + 1
 
 	def close(self):
 		self.cursor.close()
@@ -277,8 +284,8 @@ if __name__ == '__main__':
 	if id % 2 == 0:
 		gender = False
 		even = True
-	thrOffset = int(sys.argv[2])
-	id += filler.genOffset()
+	thr_offset = int(sys.argv[2])
+	id += filler.gen_offset()
 	start = time.time()
 	time.sleep(random.randint(0, 90))
 	if even:
@@ -286,14 +293,14 @@ if __name__ == '__main__':
 			filler.fill_student(id * 100_000, gender)
 			filler.fill_instructor(id * 10_000)
 			filler.class_(id * 30_000)
-			id += thrOffset
+			id += thr_offset
 			gender = not gender
 	else:
 		while time.time() - start < 3600 * 2:
 			filler.class_(id * 30_000)
 			filler.fill_instructor(id * 10_000)
 			filler.fill_student(id * 100_000, gender)
-			id += thrOffset
+			id += thr_offset
 			gender = not gender
 	if taker < 54:
 		filler.teaches(taker)
