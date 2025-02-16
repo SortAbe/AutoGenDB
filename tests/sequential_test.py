8b75cc7483e461a303042ee2a34dae8337ed92d3
Abe Diaz <aberoddia@gmail.com>
2025-02-15 23:34:53 -0500

    Changed the schema diagram to reflect current schema.

--- tests/sequential_test.py
+++ tests/sequential_test.py
@@ -10,26 +10,31 @@ connection = connector.connect(
     user='py',
     host='localhost',
     database='University',
-    password=os.environ.get('dbpass'),
+    password='xKHOxyThyC7u8f',
     port='3306',
     connect_timeout=3600,
 )
 cursor = connection.cursor()
 
 then = time.time()
-for _ in range(5000):
+for _ in range(3000):
     cursor.execute(
         f'SELECT * FROM student WHERE ID = {random.randint(0, 1_600_000)};'
     )
     cursor.fetchone()
-for _ in range(5000):
+for _ in range(3000):
     cursor.execute(
         f'SELECT * FROM sContact WHERE ID = {random.randint(0, 1_600_000)};'
     )
     cursor.fetchone()
-for _ in range(5000):
+for _ in range(3000):
     cursor.execute(
         f'SELECT * FROM sAddress WHERE ID = {random.randint(0, 1_600_000)};'
+    )
+    cursor.fetchone()
+for _ in range(3000):
+    cursor.execute(
+        f'SELECT * FROM student JOIN sAddress on student.ID = sAddress.ID JOIN sContact on student.ID = sContact.ID WHERE student.ID = {random.randint(0, 1_600_000)};'
     )
     cursor.fetchone()
 for _ in range(500):
